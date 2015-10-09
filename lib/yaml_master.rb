a51831bd38454580fec258e918a585e7aff34311
joker1007 <kakyoin.hierophant@gmail.com>
2015-10-09 12:08:47 +0900

    Add embedded helper methods

--- lib/yaml_master.rb
+++ lib/yaml_master.rb
@@ -2,16 +2,20 @@ require "yaml_master/version"
 
 require "yaml"
 require "erb"
+require "pathname"
 
 class YamlMaster
-  attr_reader :master
+  attr_reader :master, :master_path
 
   def initialize(io_or_filename)
+    embedded_methods = EmbeddedMethods.new(self)
+    embedded_methods_binding = embedded_methods.instance_eval { binding }
     data =
       if io_or_filename.is_a?(IO)
-        ERB.new(io_or_filename.read).result
+        ERB.new(io_or_filename.read).result(embedded_methods_binding)
       else
-        ERB.new(File.read(io_or_filename)).result
+        @master_path = File.expand_path(io_or_filename)
+        ERB.new(File.read(io_or_filename)).result(embedded_methods_binding)
       end
 
     @master = YAML.load(data)
@@ -28,6 +32,25 @@ class YamlMaster
   def generate_all
     @master["yaml_master"].each do |key, output|
       generate(key, output)
+    end
+  end
+
+  class EmbeddedMethods
+    def initialize(yaml_master)
+      @yaml_master = yaml_master
+    end
+
+    def master_path
+      Pathname(@yaml_master.master_path)
+    end
+
+    def user_home
+      Pathname(ENV["HOME"])
+    end
+
+    def read_file_if_exist(path)
+      return nil unless File.exist?(path)
+      File.read(path)
     end
   end
 end
--- spec/yaml_master_spec.rb
+++ spec/yaml_master_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 RSpec.describe YamlMaster do
   let!(:yaml_master) { YamlMaster.new(File.expand_path("../sample.yml", __FILE__)) }
 
+  after do
+    FakeFS.deactivate!
+  end
+
   it "#generate_all" do
     FakeFS.activate!
 
@@ -10,10 +14,15 @@ RSpec.describe YamlMaster do
     yaml_master.generate_all
     expect(File.exist?("./database.yml")).to be_truthy
 
-    config = YAML.load_file("./database.yml")
-    expect(config["development"]["adapter"]).to eq "mysql2"
-    expect(config["test"]["adapter"]).to eq "mysql2"
-    expect(config["production"]["adapter"]).to eq "mysql2"
+    yaml1 = YAML.load_file("./database.yml")
+    expect(yaml1["development"]["adapter"]).to eq "mysql2"
+    expect(yaml1["test"]["adapter"]).to eq "mysql2"
+    expect(yaml1["production"]["adapter"]).to eq "mysql2"
+
+    yaml2 = YAML.load_file("./embedded_methods.yml")
+    expect(yaml2["master_path"]).to eq File.expand_path("../sample.yml", __FILE__)
+    expect(yaml2["user_home"]).to eq ENV["HOME"]
+    expect(yaml2["read_file_if_exist"]).to match /dummy/
 
     FakeFS.deactivate!
   end
