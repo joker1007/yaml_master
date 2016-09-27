8a6ff189df0ae1bd1173dc27c1e8a98544b56b3d
joker1007 <kakyoin.hierophant@gmail.com>
2016-09-27 20:37:17 +0900

    Add helper method "include_yaml" and refactor codes

--- lib/yaml_master.rb
+++ lib/yaml_master.rb
@@ -15,23 +15,12 @@ class YamlMaster
       super("cannot fetch key \"#{key}\" from\n#{data.pretty_inspect}")
     end
   end
-  class PropertyParseError < StandardError; end
-
   attr_reader :master, :master_path, :properties
 
   def initialize(io_or_filename, property_strings = [])
-    parse_properties(property_strings)
+    @context = Context.new(io_or_filename, PropertyParser.parse_properties(property_strings))
 
-    embedded_methods_binding = EmbeddedMethods.new(self).get_binding
-    data =
-      if io_or_filename.is_a?(IO)
-        ERB.new(io_or_filename.read).result(embedded_methods_binding)
-      else
-        @master_path = File.expand_path(io_or_filename)
-        ERB.new(File.read(io_or_filename)).result(embedded_methods_binding)
-      end
-
-    @master = YAML.load(data)
+    @master = YAML.load(@context.render_master)
     raise "yaml_master key is necessary on toplevel" unless @master["yaml_master"]
     raise "data key is necessary on toplevel" unless @master["data"]
   end
@@ -76,30 +65,33 @@ class YamlMaster
     end
   end
 
-  def parse_properties(property_strings)
-    @properties = {}
-    if property_strings.is_a?(Hash)
-      @properties = property_strings
-    else
-      property_strings.each_with_object(@properties) do |str, hash|
-        key, value = str.split("=")
-        raise PropertyParseError.new("#{str} is invalid format") unless key && value
-        hash[key] = value
+  module PropertyParser
+    class ParseError < StandardError; end
+
+    def self.parse_properties(property_strings_or_hash)
+      if property_strings_or_hash.is_a?(Hash)
+        property_strings_or_hash
+      else
+        property_strings_or_hash.each_with_object({}) do |str, hash|
+          key, value = str.split("=")
+          raise ParseError.new("#{str} is invalid format") unless key && value
+          hash[key] = value
+        end
       end
     end
   end
 
-  class EmbeddedMethods
-    def initialize(yaml_master)
-      @yaml_master = yaml_master
-    end
+  class Context
+    attr_reader :master_path, :properties
 
-    def master_path
-      Pathname(@yaml_master.master_path)
-    end
-
-    def properties
-      @yaml_master.properties
+    def initialize(io_or_master_file, properties)
+      case io_or_master_file
+      when String
+        @master_path = Pathname(io_or_master_file)
+      when File
+        @master_path = Pathname(io_or_master_file.absolute_path)
+      end
+      @properties = properties
     end
 
     def user_home
@@ -111,8 +103,25 @@ class YamlMaster
       File.read(path)
     end
 
-    def get_binding
-      binding
+    def include_yaml(filename)
+      YAML.dump(
+        YAML.load(render_yaml(File.expand_path(filename, File.dirname(@master_path)))),
+        canonical: true
+      ).each_line.drop(1).join
+    end
+
+    def render_master
+      render_yaml(@master_path)
+    end
+
+    private
+
+    def render_yaml(io_or_filename)
+      if io_or_filename.respond_to?(:read)
+        ERB.new(io_or_filename.read).result(binding)
+      else
+        ERB.new(File.read(io_or_filename)).result(binding)
+      end
     end
   end
 end
